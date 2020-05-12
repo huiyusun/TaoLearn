aa6f3e7cfa0f2820983cd4cb4e62f2b0417175e6
Huiyu Sun <huiyu.sun@hotmail.com>
2020-05-12 13:37:56 -0700

    added dependencies

---
+++ protobufs-default/.makepp/dna.pb.cc.mk
@@ -0,0 +1,17 @@
+IMPLICIT_TARGETS=
+SORTED_DEPS=protobufs-default/dna.proto/usr/bin/protoc
+IMPLICIT_DEPS=
+BUILD_SIGNATURE=1589306338,173081
+META_DEPS=/usr/bin/protoc
+CWD=..
+DEP_SIGS=1589306135,18221522400922,14496
+SIGNATURE=1589306338,173081
+INCLUDES=protobufs-default/dna.pb.h
+ARCH=x86_64-linux-gnu-thread-multi
+INCLUDE_SFXS=
+INCLUDE_PATHS=
+COMMAND=protoc --cpp_out=. protobufs-default/dna.proto
+C_MD5_SUM=jm31D6Flc8O0nd4u6VbLBQ
+SYSTEM_INCLUDES=algorithm google/protobuf/descriptor.h google/protobuf/generated_message_reflection.h google/protobuf/io/coded_stream.h google/protobuf/reflection_ops.h google/protobuf/stubs/common.h google/protobuf/stubs/once.h google/protobuf/stubs/port.h google/protobuf/wire_format.h google/protobuf/wire_format_lite_inl.h
+LIBS=
+END=
